53fbb5868dc206dfe70b52852b74f2a00447b3f6
islam bouterbiat <islambouter2000@gmail.com>
2022-04-21 03:54:17 +0100

    added circular progress bar

---
+++ components/progress circle/ProgressCircle.tsx
@@ -0,0 +1,24 @@
+import { Progress } from 'antd'
+import 'antd/dist/antd.css'
+
+interface ProgressCircleProps {
+  percent: number
+}
+
+const ProgressCircle: React.FC<ProgressCircleProps> = ({ percent }) => {
+  return (
+    <div className="relative -top-24 rounded-full bg-quizpage_bg p-2">
+      <Progress
+        className="mb-3"
+        type="circle"
+        strokeWidth={10}
+        // trailColor="#151515"
+        width={100}
+        //   strokeLinecap="square"
+        strokeColor="#32CCA7"
+        percent={percent}
+      />
+    </div>
+  )
+}
+export default ProgressCircle
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,10 +2,12 @@ import { useRouter } from 'next/router'
 import Image from 'next/image'
 
 import rc_logo from '../assets/images/rc_logo.png'
+import ProgressCircle from '../components/progress circle/ProgressCircle'
 
 const styles = {
   quizpage_header:
     'quizpage_header pt-12 pb-28 flex flex-col items-center justify-center',
+  header_title: 'w-[40rem] text-center text-3xl font-semibold text-white',
 }
 
 interface QuizPageProps {}
@@ -16,12 +18,15 @@ const QuizPage: React.FC<QuizPageProps> = ({}) => {
   return (
     <div className="min-h-screen bg-quizpage_bg text-white">
       <header className={styles.quizpage_header}>
-        <Image height={50} width={172} src={rc_logo} className="object-cover" />
+        <Image height={30} width={102} src={rc_logo} className="object-cover" />
         <p className="mb-4 mt-8 font-medium">Audit your landing page</p>
-        <h1 className="w-[40rem] text-center text-3xl font-semibold">
+        <h1 className={styles.header_title}>
           Is your page structured to address the following 7 key elements?
         </h1>
       </header>
+      <div className="flex justify-center items-center flex-col">
+        <ProgressCircle percent={50} />
+      </div>
     </div>
   )
 }
